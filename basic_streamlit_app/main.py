a001619f7dad1459fd645d1388ed064b62e42495
James Chen <jamesziyuan@gmail.com>
2025-02-09 18:00:52 -0500

    Update main.py

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+st.title("Welcome to my first Streamlit app")
+st.write("The Streamlit app helps to trasnform Python scripts into interactive web apps")
+
+file_path = "/Users/jameschen/Downloads/penguins.csv"
+df=pd.read_csv(file_path)
+st.dataframe(df.head())
+
+species = st.selectbox("Select a species", df["species"].unique())
+filtered_df = df[df["species"] == species]
+st.write(f"Plants in {species}:")
+
+st.dataframe(filtered_df)
+
 
